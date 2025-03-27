1a43594bf4ee4f30ee893c35e2a932f3fc2dd8c8
nathanhoehndorf <hoehndorf.nathan@gmail.com>
2025-03-27 15:40:17 -0600

    mkdirSplitpath first draft

--- mkdirSplitpath.c
+++ mkdirSplitpath.c
@@ -4,17 +4,6 @@
 extern struct NODE* root;
 extern struct NODE* cwd;
 
-//make directory
-void mkdir(char pathName[]){
-
-    // TO BE IMPLEMENTED
-    //
-    // YOUR CODE TO REPLACE THE PRINTF FUNCTION BELOW
-
-
-
-    return;
-}
 
 struct NODE* searchChild(struct NODE* parent, const char* name) {
     struct NODE* current = parent->childPtr;
@@ -26,6 +15,55 @@ struct NODE* searchChild(struct NODE* parent, const char* name) {
     }
 
     return NULL;
+}
+
+//make directory
+void mkdir(char pathName[]){
+
+    // TO BE IMPLEMENTED
+    //
+    // YOUR CODE TO REPLACE THE PRINTF FUNCTION BELOW
+
+    if (strcmp(pathName, "/") == 0 || strlen(pathName) == 0) {
+        printf("MKDIR ERROR: no path provided\n");
+        return;
+    }
+
+    char baseName[64], dirName[128];
+    struct NODE* parent = splitPath(pathName, baseName, dirName);
+
+    if (!parent) return;
+
+    struct NODE* exists = searchChild(parent, baseName);
+    if (exists) {
+        printf("MKDIR ERROR: directory already exists\n");
+        return;
+    }
+
+    struct NODE* newNode = (struct NODE*)malloc(sizeof(struct NODE));
+    if (!newNode) {
+        perror("MKDIR ERROR: memory allocation failed");
+        return;
+    }
+
+    strcpy(newNode->name, baseName);
+    newNode->fileType = 'D';
+    newNode->parentPtr = parent;
+    newNode->childPtr = NULL;
+    newNode->siblingPtr = NULL;
+
+    if (!parent->childPtr) {
+        parent->childPtr = newNode;
+    } else {
+        struct NODE* temp = parent->childPtr;
+        while (temp->siblingPtr) {
+            temp = temp->siblingPtr;
+        }
+        temp->siblingPtr = newNode;
+    }
+
+    printf("MKDIR SUCCESS: node %s successfully created\n", pathName);
+    
 }
 
 //handles tokenizing and absolute/relative pathing options
